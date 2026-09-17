6ac55bceb9e53fb8c017058bc9d2221336630103
jagadeeshbabu-ai <boddujagadeesh225@gmail.com>
2026-09-17 11:50:21 +0530

    Added Pandas employee filtering practice

--- task.py
+++ task.py
@@ -23,3 +23,13 @@ result = df[
 result = result[[ "Name", "Department", "Experience", "Salary"]]
 
 print(result)
+print("============================")
+g= df.loc[
+    (df["Department"].isin(["IT", "Finance"])) &
+    (df["Experience"] >= 5) &
+    (df["Age"].between(25, 35)) &
+    (df["Salary"].between(55000, 90000)),
+    [ "Name", "Department", "Experience", "Salary"]
+]
+
+print(g)
